acc4303f936fc427f849804f87c461cf55d63742
Karlheinz Huscher Cirne <66499199+kcirne25@users.noreply.github.com>
2023-10-22 09:48:35 -0400

    Update process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -1,162 +1,90 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # ETL Pipeline Preparation
-# Follow the instructions below to help you create your ETL pipeline.
-# ### 1. Import libraries and load datasets.
-# - Import Python libraries
-# - Load `messages.csv` into a dataframe and inspect the first few lines.
-# - Load `categories.csv` into a dataframe and inspect the first few lines.
-
-# In[1]:
-
-
 # import libraries
 import pandas as pd
-
 from sqlalchemy import create_engine
 
+def load_data(messages_filepath, categories_filepath):
+    """
+    Load messages and categories datasets.
 
-# In[2]:
+    Args:
+        messages_filepath (str): Filepath of messages dataset.
+        categories_filepath (str): Filepath of categories dataset.
 
+    Returns:
+        tuple: A tuple containing two dataframes (messages, categories).
+    """
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    return messages, categories
 
-# load messages dataset
-messages = pd.read_csv('messages.csv')
-messages.head()
+def merge_data(messages, categories):
+    """
+    Merge messages and categories datasets.
 
+    Args:
+        messages (pd.DataFrame): Messages dataframe.
+        categories (pd.DataFrame): Categories dataframe.
 
-# In[3]:
+    Returns:
+        pd.DataFrame: Merged dataframe.
+    """
+    df = pd.merge(messages, categories, on='id')
+    return df
 
+def clean_data(df):
+    """
+    Clean the merged dataframe.
 
-# load categories dataset
-categories = pd.read_csv('categories.csv')
-categories.head()
+    Args:
+        df (pd.DataFrame): Merged dataframe.
 
-
-# ### 2. Merge datasets.
-# - Merge the messages and categories datasets using the common id
-# - Assign this combined dataset to `df`, which will be cleaned in the following steps
-
-# In[4]:
-
-
-# merge datasets
-df = pd.merge(messages, categories, on = 'id')
-df.head()
-
-
-# ### 3. Split `categories` into separate category columns.
-# - Split the values in the `categories` column on the `;` character so that each value becomes a separate column. You'll find [this method](https://pandas.pydata.org/pandas-docs/version/0.23/generated/pandas.Series.str.split.html) very helpful! Make sure to set `expand=True`.
-# - Use the first row of categories dataframe to create column names for the categories data.
-# - Rename columns of `categories` with new column names.
-
-# In[5]:
-
-
-# create a dataframe of the 36 individual category columns
-categories = df['categories'].str.split(pat=';', expand = True)
-categories.head()
-
-
-# In[6]:
-
-
-# select the first row of the categories dataframe
-row = categories.iloc[0]
-
-# use this row to extract a list of new column names for categories.
-# one way is to apply a lambda function that takes everything 
-# up to the second to last character of each string with slicing
-category_colnames = row.apply(lambda x:x[:-2])
-print(category_colnames)
-
-
-# In[7]:
-
-
-# rename the columns of `categories`
-categories.columns = category_colnames
-categories.head()
-
-
-# ### 4. Convert category values to just numbers 0 or 1.
-# - Iterate through the category columns in df to keep only the last character of each string (the 1 or 0). For example, `related-0` becomes `0`, `related-1` becomes `1`. Convert the string to a numeric value.
-# - You can perform [normal string actions on Pandas Series](https://pandas.pydata.org/pandas-docs/stable/text.html#indexing-with-str), like indexing, by including `.str` after the Series. You may need to first convert the Series to be of type string, which you can do with `astype(str)`.
-
-# In[8]:
-
-
-for column in categories:
-    # set each value to be the last character of the string
-    categories[column] = categories[column].astype(str).str[-1]
+    Returns:
+        pd.DataFrame: Cleaned dataframe.
+    """
+    # Create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(';', expand=True)
     
-    # convert column from string to numeric
-    categories[column] = categories[column].astype(int)
-categories.head()
+    # Use the first row of the categories dataframe to create column names for the categories data
+    row = categories.iloc[0]
+    category_colnames = row.apply(lambda x: x[:-2])
 
+    # Rename columns of categories with new column names
+    categories.columns = category_colnames
 
-# ### 5. Replace `categories` column in `df` with new category columns.
-# - Drop the categories column from the df dataframe since it is no longer needed.
-# - Concatenate df and categories data frames.
+    for column in categories:
+        # Set each value to be the last character of the string
+        categories[column] = categories[column].astype(str).str[-1]
+        # Convert column from string to numeric
+        categories[column] = categories[column].astype(int)
 
-# In[9]:
+    # Drop the original categories column from df
+    df = df.drop('categories', axis=1)
 
+    # Concatenate the original dataframe with the new categories dataframe
+    df = pd.concat([df, categories], axis=1)
 
-# drop the original categories column from `df`
-df = df.drop('categories', axis = 1)
+    # Drop duplicates
+    df.drop_duplicates(inplace=True)
 
-df.head()
+    return df
 
+def save_data(df, database_filename):
+    """
+    Save the cleaned dataset into an sqlite database.
 
-# In[10]:
+    Args:
+        df (pd.DataFrame): Cleaned dataframe.
+        database_filename (str): Name of the database file.
+    """
+    engine = create_engine(f'sqlite:///{database_filename}')
+    df.to_sql('DisasterResponse_table', engine, index=False)
 
+# Example usage
+messages_filepath = 'messages.csv'
+categories_filepath = 'categories.csv'
+database_filename = 'DisasterResponse.db'
 
-# concatenate the original dataframe with the new `categories` dataframe
-df = pd.concat([df, categories], axis =1)
-df.head()
-
-
-# ### 6. Remove duplicates.
-# - Check how many duplicates are in this dataset.
-# - Drop the duplicates.
-# - Confirm duplicates were removed.
-
-# In[11]:
-
-
-# check number of duplicates
-df.duplicated().sum()
-
-
-# In[12]:
-
-
-# drop duplicates
-df.drop_duplicates(inplace = True)
-
-
-# In[13]:
-
-
-# check number of duplicates
-df.duplicated().sum()
-
-
-# ### 7. Save the clean dataset into an sqlite database.
-# You can do this with pandas [`to_sql` method](https://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.to_sql.html) combined with the SQLAlchemy library. Remember to import SQLAlchemy's `create_engine` in the first cell of this notebook to use it below.
-
-# In[14]:
-
-
-engine = create_engine('sqlite:///InsertDatabaseName.db')
-df.to_sql('DisasterResponse_table', engine, index=False)
-
-
-# ### 8. Use this notebook to complete `etl_pipeline.py`
-# Use the template file attached in the Resources folder to write a script that runs the steps above to create a database based on new datasets specified by the user. Alternatively, you can complete `etl_pipeline.py` in the classroom on the `Project Workspace IDE` coming later.
-
-# In[ ]:
-
-
-
-
+messages, categories = load_data(messages_filepath, categories_filepath)
+df = merge_data(messages, categories)
+cleaned_df = clean_data(df)
+save_data(cleaned_df, database_filename)
